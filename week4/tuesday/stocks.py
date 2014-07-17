a9dcb1d45242ba2c7444e6dcf485bf8864f796cc
Kevin Ernest Long <kevinelong@gmail.com>
2014-07-16 18:28:56 -0700

    BASELINE STOCK PRICE LISTING

--- week4/tuesday/stocks.py
+++ week4/tuesday/stocks.py
@@ -1,13 +1,19 @@
+#BASELINE STOCK PRICE LISTING APPLICATION
+
 class Stock():
     def __init__(self, ticker_symbol):
         self.ticker_symbol = ticker_symbol
         self.historical_quotes = []
     def moving_average(self):
         pass
+
+
 class Quote():
     def __init__(self, closing_price, quote_date):
         self.closing_price = closing_price
         self.quote_date = quote_date
+
+
 class StocksModel():
     stocks = {}
     def load(self, data):
@@ -19,19 +25,32 @@ class StocksModel():
                 self.stocks[symbol] = Stock(symbol)
             stock = self.stocks[symbol]
             stock.historical_quotes.append( Quote(price, date) )
+
+
 class StocksView():
     def render(self,stocks):
-        for k,v in stocks:
+        for k in stocks.keys():
             print k
-            for q in s.historical_quotes:
-                print q.quote_date + "-" + q.closing_price
+            v = stocks[k]
+
+            for q in v.historical_quotes:
+                #HOW CAN WE PRINT THE DIFFERENCE BETWEEN THIS PRICE AND THE PREVIOUS PRICE
+                print q.quote_date + " " + str(q.closing_price)
+
+
 test_data = [
-    ["2014-06-01", "APPL",100.10],
-    ["2014-06-02", "APPL",110.60],
-    ["2014-06-03", "APPL",120.20],
-    ["2014-06-04", "APPL",100.50],
+    ["2014-06-01", "APPL",100.11],
+    ["2014-06-02", "APPL",110.61],
+    ["2014-06-03", "APPL",120.22],
+    ["2014-06-04", "APPL",100.54],
+    ["2014-06-01", "ATT",20.46],
+    ["2014-06-02", "ATT",21.25],
+    ["2014-06-03", "ATT",32.53],
+    ["2014-06-04", "ATT",40.71],
 ]
+
 model = StocksModel()
 model.load(test_data)
+
 view = StocksView()
 view.render(model.stocks)
