e7883198ac492b2dd24490be2bee02afed430391
Kevin Ernest Long <kevinelong@gmail.com>
2014-07-25 18:55:00 -0700

    form response data and ascii

---
+++ week5/letters.py
@@ -0,0 +1,21 @@
+for i in range(255):
+    print( str(i) + "=" + chr( i ) )
+
+#ASCII - Plain Ordinary English Text
+#American Standard Code for Information Interchange
+
+# ascii = {
+#     "\r": 10,
+#     "\n": 13,
+#     " ": 32,
+#     "A": 65,
+#     "B": 66,
+#     ...
+# }
+
+# {
+#     1: "00001000",
+#     2: "00001000",
+#     3: "00001000",
+#     ...
+# }
