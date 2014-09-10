9b5724469f2d6c48cbecee0ae4f539e7c9bcf0e9
Kevin Ernest Long <kevinelong@gmail.com>
2014-09-09 21:19:10 -0700

    stacks solution one

--- week11/stacks/stacks.py
+++ week11/stacks/stacks.py
@@ -1,6 +1,4 @@
 loose_change = [
-    {"denomination":"quarter","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
     {"denomination":"nickel","year":"2014"},
     {"denomination":"dime","year":"2014"},
     {"denomination":"nickel","year":"2014"},
@@ -9,14 +7,12 @@ loose_change = [
     {"denomination":"nickel","year":"2014"},
     {"denomination":"dime","year":"2014"},
     {"denomination":"nickel","year":"2014"},
+    {"denomination":"nickel","year":"2014"},
+    {"denomination":"dime","year":"2014"},
+    {"denomination":"nickel","year":"2014"},
     {"denomination":"quarter","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
-    {"denomination":"dime","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
-    {"denomination":"dime","year":"2014"},
-    {"denomination":"nickel","year":"2014"},
+    {"denomination":"quarter","year":"2014"},
+    {"denomination":"quarter","year":"2014"},
     {"denomination":"quarter","year":"2014"},
     {"denomination":"nickel","year":"2014"},
     {"denomination":"nickel","year":"2014"},
@@ -24,3 +20,22 @@ loose_change = [
     {"denomination":"nickel","year":"2014"},
 ]
 # How many stacks of N units can we make for each denomination of coin?
+
+grouped = {}
+stack_limit = 4
+
+for coin in loose_change:
+    denomination = coin["denomination"]
+    if denomination not in grouped.keys():
+        grouped[denomination] = [ [] ]
+    last_stack = grouped[denomination][-1]
+    if len(last_stack) < stack_limit:
+        last_stack.append(coin)
+    else:
+        new_stack = []
+        new_stack.append( coin)
+        grouped[denomination].append(new_stack)
+
+for denomination in grouped:
+    stacks = grouped[denomination]
+    print(str(len(stacks)) + " of " + denomination)
